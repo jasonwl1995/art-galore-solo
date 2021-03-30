8ed200841895f3aec369c547a95f9f0b78230247
Jason Lin <jasonwl_1995@yahoo.com>
2021-03-30 11:12:30 -0500

    test

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -23,7 +23,7 @@ import RegisterPage from '../RegisterPage/RegisterPage';
 import './App.css';
 
 // Import created components
-import DiscoverGalleryPage from '../DiscoverGalleryPage/DiscoverGalleryPage.jsx';
+import DiscoverGalleryPage from '../DiscoverGalleryPage/DiscoverGalleryPage';
 import MyGalleryPage from '../MyGalleryPage/MyGalleryPage';
 import EditArtworkPage from '../EditArtworkPage/EditArtworkPage';
 import MyArtworkDetail from '../MyArtworkDetail/MyArtworkDetail';
