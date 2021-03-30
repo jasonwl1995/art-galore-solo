b373fba2a2763a8a9a281bb721bbadadfe1809fc
Jason Lin <jasonwl_1995@yahoo.com>
2021-03-30 10:36:47 -0500

    test

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -15,7 +15,7 @@ import Footer from '../Footer/Footer';
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 
 import AboutPage from '../AboutPage/AboutPage';
-import ProfilePage from './ProfilePage/ProfilePage';
+import ProfilePage from '../ProfilePage/ProfilePage';
 import InfoPage from '../InfoPage/InfoPage';
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
